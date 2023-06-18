8ad2371e31008bc135683b0f76c5ad5fe07348ea
Hoàng Ngọc Thuỷ <91459703+Shyn-K@users.noreply.github.com>
2023-06-18 22:56:41 +0700

    Update Employee.java

--- src/sellingonlinecoursesmanagement/Entity/Person/Employee/Employee.java
+++ src/sellingonlinecoursesmanagement/Entity/Person/Employee/Employee.java
@@ -1,5 +1,10 @@
 package sellingonlinecoursesmanagement.Entity.Person.Employee;
 
-public class Employee {
-    
+public class Employee extends Person {
+    public Employee() {
+    }
+
+    public Employee(String personID, String name, int age, String gender, String phoneNumber, String email) {
+        super(personID, name, age, gender, phoneNumber, email);
+    }
 }
