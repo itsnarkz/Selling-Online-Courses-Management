b6f0f803445762798f271c91e909edf4866ff3c7
Hoàng Ngọc Thuỷ <91459703+Shyn-K@users.noreply.github.com>
2023-06-18 22:57:01 +0700

    Update Manager.java

--- src/sellingonlinecoursesmanagement/Entity/Person/Manager/Manager.java
+++ src/sellingonlinecoursesmanagement/Entity/Person/Manager/Manager.java
@@ -1,4 +1,13 @@
 package sellingonlinecoursesmanagement.Entity.Person.Manager;
 
-public class Manager {
+public class Manager extends Person{
+    public Manager() {
+    }
+
+    public Manager(String personID, String name, int age, String gender, String phoneNumber, String email) {
+        super(personID, name, age, gender, phoneNumber, email);
+    }
+
+
 }
+
