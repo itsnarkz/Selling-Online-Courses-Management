6735da8d456596166de858203e2cc37a3abd16de
Narkz <aninatsumi@gmail.com>
2023-06-27 17:05:43 +0700

    init customer

--- src/sellingonlinecoursesmanagement/Entity/Person/Customer/Customer.java
+++ src/sellingonlinecoursesmanagement/Entity/Person/Customer/Customer.java
@@ -1,12 +1,5 @@
 package sellingonlinecoursesmanagement.Entity.Person.Customer;
 
 public class Customer{
-    private String voucher;
 
-    public Customer() {
-    }
-
-    public Customer(String personID, String name, int age, String gender, String phoneNumber, String email, String voucher) {
-        this.voucher = voucher;
-    }
 }
