80ffd2a1dbf35257d8d3a0b7cf90c18d5992e74e
Hoàng Ngọc Thuỷ <91459703+Shyn-K@users.noreply.github.com>
2023-06-27 09:35:58 +0700

    Update Order.java

--- src/sellingonlinecoursesmanagement/Entity/Order/Order.java
+++ src/sellingonlinecoursesmanagement/Entity/Order/Order.java
@@ -1,93 +1,70 @@
 package sellingonlinecoursesmanagement.Entity.Order;
 
+import sellingonlinecoursesmanagement.Entity.Course.Course;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Order {
     private String orderId;
     private String customerName;
     private LocalDateTime orderDate;
-    private String status;
+    private List<Course> courses;
     private double cost;
-
-    public Order(String orderId, String customerName, LocalDateTime orderDate, String status, double cost) {
-        this.orderId = orderId;
-        this.customerName = customerName;
-        this.orderDate = orderDate;
-        this.status = status;
-        this.cost = cost;
-    }
+    private String status;
 
     public Order(String orderId, String customerName) {
+        this.orderId = orderId;
+        this.customerName = customerName;
+        this.orderDate = LocalDateTime.now();
+        this.courses = new ArrayList<>();
+        this.cost = 0.0;
+        this.status = "In Progress";
     }
 
     public String getOrderId() {
         return orderId;
     }
 
-    public void setOrderId(String orderId) {
-        this.orderId = orderId;
-    }
-
     public String getCustomerName() {
         return customerName;
-    }
-
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
     }
 
     public LocalDateTime getOrderDate() {
         return orderDate;
     }
 
-    public void setOrderDate(LocalDateTime orderDate) {
-        this.orderDate = orderDate;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
+    public List<Course> getCourses() {
+        return courses;
     }
 
     public double getCost() {
         return cost;
     }
 
+    public String getStatus() {
+        return status;
+    }
+
     public void setCost(double cost) {
         this.cost = cost;
     }
 
-
-    //method display
-    public void displayOrder(String orderID) {
-//        System.out.println("--------------------------------------------------------------------------------------------");
-//        System.out.println("|                                                                                          |");
-//        System.out.println("|                               Super  Vip  Pro                                            |");
-//        System.out.println("|                                                                                          |");
-//        System.out.println("| ---------------------------------------------------------------------------------------- |");
-//        System.out.format("| Order ID: %79s|\n", orderID.getOrderId());
-//        displayDate(orderID.getOrderDate());
-//        System.out.format("| Name:    %80s|\n", orderID.getCustomerName());
-//        System.out.format("| Status:  %80s|\n", orderID.getStatus());
-//        System.out.println("| ---------------------------------------------------------------------------------------- |");
-//        System.out.println("|   No  |                     Course                    |   Author   | Quantity |   Cost   |");
-//        System.out.println("| ---------------------------------------------------------------------------------------- |");
-//        // Print Entity.course details (dummy data for demonstration)
-//        System.out.println("|   1   | Course 1                                      |  Author 1  |    2     |  $100.00 |");
-//        System.out.println("|   2   | Course 2                                      |  Author 2  |    1     |  $50.00  |");
-//        System.out.println("|   3   | Course 3                                      |  Author 3  |    3     |  $150.00 |");
-//        // Print total cost
-//        System.out.println("| ---------------------------------------------------------------------------------------- |");
-//        System.out.format("| Total Cost: %-77.2f|\n", orderID.getCost());
-//        System.out.println("| ---------------------------------------------------------------------------------------- |");
+    public void addCourse(Course course) {
+        courses.add(course);
     }
 
-    private void displayDate(LocalDateTime orderDate) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
-        System.out.format("| Order Date: %76s|\n", orderDate.format(formatter));
+    public void displayOrder(String orderID) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        System.out.println("Order ID: " + orderID);
+        System.out.println("Customer Name: " + customerName);
+        System.out.println("Order Date: " + orderDate.format(formatter));
+        System.out.println("Cost: " + cost);
+        System.out.println("Status: " + status);
+        System.out.println("Courses in Order:");
+        for (Course course : courses) {
+            System.out.println("- " + course.getCourseName());
+        }
     }
 }
