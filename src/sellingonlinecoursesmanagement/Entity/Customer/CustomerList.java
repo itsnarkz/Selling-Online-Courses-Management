f4e11d098e689fd9ddf3739347fe5c389a94f075
Hoàng Ngọc Thuỷ <91459703+Shyn-K@users.noreply.github.com>
2023-06-29 18:39:42 +0700

    Add files via upload

---
+++ src/sellingonlinecoursesmanagement/Entity/Customer/CustomerList.java
@@ -0,0 +1,87 @@
+package sellingonlinecoursesmanagement.Entity.Person.Customer;
+
+import sellingonlinecoursesmanagement.Entity.Course.Course;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class CustomerList {
+    private List<Customer> customerList;
+
+    public CustomerList() {
+        this.customerList = new ArrayList<Customer>();
+    }
+
+    public void createCustomer(String id, String name, int age, String gender, String phoneNumber, String email) {
+        Customer newCustomer = new Customer(id,name,age,gender,phoneNumber,email);
+
+        boolean check = false;
+        for(Customer customer : customerList) {
+            if(Objects.equals(customer.getId(), id)) {
+                check = true;
+                break;
+            }
+        }
+
+        if(!check) customerList.add(newCustomer);
+    }
+
+
+    public void deleteCustomer(String id) {
+        Customer target = null;
+        for(Customer customer : customerList) {
+            if(Objects.equals(customer.getId(), id)) {
+                target = customer;
+                break;
+            }
+        }
+
+        if(target != null) customerList.remove(target);
+    }
+
+
+    public Customer searchByID(String id) {
+        Customer target = null;
+        for(Customer customer : customerList) {
+            if(Objects.equals(customer.getId(), id)) {
+                target = customer;
+                break;
+            }
+        }
+
+        return target;
+    }
+
+
+    public void updateCustomer(String id, String name, int age, String gender, String phoneNumber, String email) {
+        Customer target = null;
+        for(Customer customer : customerList) {
+            if(Objects.equals(customer.getId(), id)) {
+                target = customer;
+                break;
+            }
+        }
+
+        if(target == null) System.out.println("Can not found customer with ID " + id + "!");
+        else {
+            if(!Objects.equals(name, "0")) target.setName(name);
+            if(age != 0) target.setAge(age);
+            if(!Objects.equals(gender, "0")) target.setGender(gender);
+            if(!Objects.equals(phoneNumber, "0")) target.setPhoneNumber(phoneNumber);
+            if(!Objects.equals(email, "0")) target.setEmail(email);
+        }
+    }
+
+
+    public List<Customer> listAllCustomer() {
+        return this.customerList;
+    }
+
+    public void display(List<Customer> list) {
+        System.out.printf("%15s %20s %20s %20s %20s %15s %15s %15s %20s %20s\n", "CourseID", "Name", "Category", "Major", "Author", "Sold", "Price", "Rating", "Published Date", "Last Update Date");
+        for(Customer customer : list) customer.display();
+    }
+
+}
